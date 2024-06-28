8f3b21f8f9c933f082bbc8f2cd0bee784c7e84de
Eric Laursen <ericjlaursen@gmail.com>
2024-06-27 20:08:29 -0400

    change import syntax

--- pages/ml-ui/face-detection-with-webcamera.js
+++ pages/ml-ui/face-detection-with-webcamera.js
@@ -1,7 +1,7 @@
 import '@tensorflow/tfjs';
 import '@tensorflow/tfjs-core';
 import '@tensorflow/tfjs-backend-webgl';
-import * as faceDetection from '@tensorflow-models/face-detection';
+import faceDetection from '@tensorflow-models/face-detection';
 import { useEffect, useRef, useState } from 'react';
 
 // components
