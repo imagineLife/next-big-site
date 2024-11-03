95f2f4d657ffc30c6ae3bdb21d29a47c4c19bc24
Eric Laursen <ericjlaursen@gmail.com>
2024-11-03 07:13:08 -0500

    set prism to highlight them all!

--- pages/node/buffers.js
+++ pages/node/buffers.js
@@ -1,6 +1,12 @@
+import React, { useEffect } from 'react';
 import Buffers from './../../markdown/node/buffers.md';
 import GenericPost from './../../components/GenericPost';
+import Prism from 'prismjs';
 export default function Page() {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+
   return (
     <GenericPost
       {...{
