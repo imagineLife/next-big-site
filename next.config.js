360e95a63eb2a313bbcbfce37373887ecb043672
Eric Laursen <ericjlaursen@gmail.com>
2024-11-02 20:33:29 -0400

    more

--- next.config.js
+++ next.config.js
@@ -1,18 +1,30 @@
 // DOCS
 // https://nextjs.org/docs/pages/api-reference/next-config-js/webpack
 
+const createMDX = require('@next/mdx');
+
 // txt file
 const textLoader = {
   test: /\.txt/,
   type: 'asset/source',
 };
 
-module.exports = {
+const mdLoader = {
+  test: /\.md/,
+  use: [
+    {
+      loader: '@mdx-js/loader',
+    },
+  ],
+};
+
+const nextConfig = {
   webpack: (
     config,
     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
   ) => {
     config.module.rules.push(textLoader);
+    config.module.rules.push(mdLoader);
     // Important: return the modified config
     return config;
   },
@@ -25,4 +37,13 @@ module.exports = {
       },
     ],
   },
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
+
+// Add markdown plugins here, as desired
+const withMDX = createMDX({});
+
+// Merge MDX config with Next.js config
+// @ts-ignore
+module.exports = withMDX(nextConfig);
--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -19,23 +19,84 @@ import remarkGfm from 'remark-gfm';
 //
 const cwd = process.cwd();
 const pages_dir = join(cwd, 'pages');
+const public_dir = join(cwd, 'public');
 
+// directories in /public/<dir-here>
 export const docker_path = join(pages_dir, 'docker');
 export const ml_path = join(pages_dir, 'ml');
 export const linux_path = join(pages_dir, 'linux');
 export const mongo_path = join(pages_dir, 'mongo');
 export const nginx_path = join(pages_dir, 'nginx');
+export const notebooks_path = join(public_dir, 'notebooks');
 export const scrum_path = join(pages_dir, 'scrum');
 export const tf_path = join(pages_dir, 'tf');
+export const node_path = join(pages_dir, 'node');
 
 function onlyMdxFile(s) {
   return /\.mdx?$/.test(s);
 }
 
+function onlyNbFiles(s) {
+  return /\.ipynb?$/.test(s);
+}
 // postsFiles is the list of all mdx files inside the posts_path directory
 export const dockerMdPaths = readdirSync(docker_path).filter(onlyMdxFile);
 export const mlMdPaths = readdirSync(ml_path).filter(onlyMdxFile);
 export const linuxMdPaths = readdirSync(linux_path).filter(onlyMdxFile);
+export const notebookPaths = readdirSync(notebooks_path).filter(onlyNbFiles);
+// export const nodeMdPaths = readdirSync(node_path).filter(onlyMdxFile);
+let nodeDirsToParse = [node_path];
+let nodeFilesToParse = [];
+let nodeMdPaths = [];
+function withParentDirPath(parentDirPath) {
+  return function pushDrillOrSkip(item, idx, originalArr) {
+    nodeFilesToParse.push(item);
+
+    if (typeof item === 'string' && item.includes('.md')) {
+      const preSlug = `${parentDirPath.path}/${parentDirPath.name}`.split(
+        'node/'
+      )[1];
+
+      nodeMdPaths.push({ params: { slug: [preSlug, item.split('.')[0]] } });
+      nodeFilesToParse.pop();
+      if (idx === originalArr.length - 1 && nodeDirsToParse.length > 0) {
+        // if (nodeDirsToParse.length == 0 && nodeFilesToParse.length === 0) {
+        //   console.log('DONE A?!');
+        //   console.log(nodeMdPaths);
+        // }
+        nodeDirsToParse.pop();
+      }
+    } else if (Boolean(item?.isDirectory) && item?.isDirectory()) {
+      const newPath = join(arguments[0].path, item?.name);
+      nodeDirsToParse.push(newPath);
+      const poppedDir = nodeFilesToParse.pop();
+      readdirSync(newPath).forEach(withParentDirPath(poppedDir));
+    } else {
+      nodeFilesToParse.pop();
+      if (idx === originalArr.length - 1) {
+        if (nodeDirsToParse.length > 0) {
+          nodeDirsToParse.pop();
+          // if (nodeDirsToParse.length == 0 && nodeFilesToParse.length === 0) {
+          //   console.log('DONE B?!');
+          //   console.log(nodeMdPaths);
+          // }
+          // } else {
+          //   console.log('DONE C?!');
+          //   console.log(nodeMdPaths.map((f) => f.params.slug));
+          // }
+        }
+      }
+    }
+  };
+}
+
+// readdirSync(nodeDirsToParse.pop(), { withFileTypes: true }).forEach(
+//   pushDrillOrSkip
+// );
+const poppedDirToParse = nodeDirsToParse.pop();
+readdirSync(poppedDirToParse, { withFileTypes: true }).forEach(
+  withParentDirPath()
+);
 
 let nestedDirs = {
   mongo: [],
@@ -95,13 +156,17 @@ const filePaths = {
   nginx: nginxMdPaths,
   scrum: scrumMdPaths,
   tf: tfMdPaths,
+  node: nodeMdPaths,
+  notebooks: notebookPaths,
   // 'mongo-aggregations': mongoAggMdPaths,
 };
 const dirPaths = {
   docker: docker_path,
+  node: node_path,
   ml: ml_path,
   linux: linux_path,
   mongo: mongo_path,
+  node: node_path,
   nginx: nginx_path,
   scrum: scrum_path,
   tf: tf_path,
@@ -123,6 +188,10 @@ export const sortPostsByDate = (posts) => {
 export const getPosts = (pathDir) => {
   if (!pathDir) throw new Error('getPosts called without a param');
   const pathFilePaths = filePaths[pathDir];
+
+  if (pathDir === 'notebooks') {
+    return pathFilePaths.map((s) => s.split('.ipynb')[0]);
+  }
 
   let posts = pathFilePaths
     .map((filePath) => {
@@ -151,10 +220,12 @@ function filenameFromSlugAndSection(slug, section, useMDX) {
     docker: (s) => `${s}.md`,
     linux: (s) => `${s}.md`,
     // 'mongo-aggregations': (s) => `${s}.md`,
+    node: (s) => `${s}.md`,
     nginx: (s) => `${s}.md`,
     scrum: (s) => `${s}.md`,
     tf: (s) => `${s}.mdx`,
   };
+  if (typeof slug === 'object') slug = slug.join('/');
   return filenameLookup[section](slug);
 }
 
@@ -228,3 +299,41 @@ export function getNestedSections(section) {
     return [];
   }
 }
+
+// async
+export function getNodeSections() {
+  // const nodeSections = readdirSync(node_path, { withFileTypes: true })
+  //   .filter((f) => f.isDirectory())
+  //   .map((s) => {
+  //     const overviewFile = join(node_path, s.name, 'overview.');
+  //     const fileContent = readFileSync(overviewFile);
+  //     const { data } = matter(fileContent);
+  //     return { ...data };
+  //   });
+  // {t: title, d: description}
+  const nodeFs = {
+    t: 'FileSystem',
+    d: "Interact with the machine's files & directories: read, write, update, & delete.",
+    url: '/node/fs',
+  };
+  const nodeBuffers = {
+    t: 'Buffers',
+    d: 'Storing temporary data',
+    url: '/node/buffers',
+  };
+  const nodeSections = [nodeFs, nodeBuffers];
+
+  // const mdxSource = await serialize(content, {
+  // Optionally pass remark/rehype plugins
+  //   mdxOptions: {
+  //     remarkPlugins: [remarkGfm],
+  //     rehypePlugins: [rehypePrism, rehypeSlug],
+  //   },
+  //   scope: data,
+  // });
+  // return { mdxSource, data }
+
+  return nodeSections;
+}
+
+export { nodeMdPaths };
