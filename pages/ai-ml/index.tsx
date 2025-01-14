a86fae6b04daac6a825cd9af93c8405e62243834
Eric Laursen <ericjlaursen@gmail.com>
2025-01-14 15:12:41 -0500

    init ai-ml

---
+++ pages/ai-ml/index.tsx
@@ -0,0 +1,41 @@
+import GenericPost from './../../components/GenericPost'
+
+export default function AiPage() { 
+  const titleText = 'An Introduction to Artificial Intelligence & Machine Learning'
+  return (<GenericPost {...{
+        title: titleText,
+        globalData: {
+          name: titleText,
+        },
+        slug: '/work',
+        tags: [
+          'ai',
+          'artificial intelligence',
+          'machine learning',
+          'python',
+          'jupyter notebooks',
+          'tensorflow'
+        ],
+        slugArr: ['ai'],
+      }}>
+        
+        <p><i>...coming soon...</i></p>
+        <h2>Python for Data Science</h2>
+        <p>some notes and examples of using python to explore & analyze data</p>
+        
+        <h2>Intro to Machine Learning</h2>
+        <p>Linear Regressions, Decision Trees, K-Means clustering, Ensemble learning (bagging & boosting), & building models.</p>
+        
+        <h2>Intro to Neural Networks</h2>
+        <p>What they are, & how to create & fine-tune them.</p>
+
+        <h2>Natural Language Processing (NLP)</h2>
+        <p>Focusing on language and words: word embeddings, transformers, LLMs, prompt engineering, Retrieval Augmented Generation (RAG).</p>
+
+        <h2>Computer Vision</h2>
+        <p>Processing images, using convolutional neural networks (CNNs).</p>
+
+        <h2>Depoloying models to the world</h2>
+        
+      </GenericPost>)
+}
